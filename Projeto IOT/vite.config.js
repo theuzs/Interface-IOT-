71c4f93f5fa3426cb56bff91852c332f01611cde
theuzs <119632707+theuzs@users.noreply.github.com>
2024-10-07 18:55:24 -0300

    Prepare to deploy

--- Projeto IOT/vite.config.js
+++ Projeto IOT/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Interface-IOT/',
+  base: '/Interface-IOT-/',
   plugins: [react()],
 })
