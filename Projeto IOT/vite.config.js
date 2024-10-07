005f828dce5089885cef683d3769a906443ddfb7
theuzs <119632707+theuzs@users.noreply.github.com>
2024-10-07 18:25:23 -0300

    test 1

--- Projeto IOT/vite.config.js
+++ Projeto IOT/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/interface-IOT-/', // Mude para o seu repositório
+});
